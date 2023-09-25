fd2dfedb65b1d85238571ba0376a1694fe70cdd7
Kazunina Anna <anyakazunina@yandex.ru>
2023-09-25 21:44:35 +0300

    generetion

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    
+    public void Starting()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     public void Exit() 
     {
         Application.Quit();
